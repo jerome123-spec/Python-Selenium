3488fe9bd52c5b313da1264758ea994f762d39de
jeromeculcul <jeromemculcul@gmail.com>
2024-08-26 20:00:11 +0800

    latest changes for generate image , no suggested replies and upload files

--- PageObjectModel/Locators/PageLocators.py
+++ PageObjectModel/Locators/PageLocators.py
@@ -5,21 +5,30 @@ class Locators():
     password_textfield_id = "password"
     login_button_id = "login-button"
     
-    #Homepage Videos Upload
+    #Normal Chat and AI Search
+    homepage_text_field = "query-area-home"
+    homepage_send_button = "send-button-home"
+    chatroom_select_ai_search = "source-button-chatroom"
+    chatroom_text_field = "query-area-chatroom"
+    chatroom_send_button = "send-button-chatroom" 
+
+    #Select Menu (Meatball Icon)
+    chatroom_select_menu_icon = "open-menu-button-chatroom"
+    chatroom_generate_image = "generate-image-toggle-button-chatroom"
+    chatroom_suggested_replies = "suggested-replies-toggle-button-chatroom"
+    
+    #Chatroom File Upload
+    chatroom_select_attachment = "attachments-button-chatroom"
+    chatroom_select_file_document = "add-file-button-chatroom"
+    chatroom_file_input = "file"
+    chatroom_upload_button = "video-upload-btn-video-file"   
+
+   #Homepage Videos Upload
     homepage_attachment_id = "attachments-button-home"
     select_attachment_id = "upload-video-button-home"
     click_to_redirect_transcoder_id = "from-computer-div"
     file_input = "file"
     Upload_button = "video-upload-btn-video-file"
 
-    #Normal Chat and AI Search
-    homepage_text_field = "query-area-home"
-    homepage_send_button = "send-button-home"
-    chatroom_select_ai_search = "source-button-chatroom"
-    chatroom_text_field = "query-area-chatroom"
-    chatroom_send_button = "send-button-chatroom"
-    chatroom_select_menu_icon = "open-menu-button-chatroom"
-    chatroom_generate_image = "generate-image-toggle-button-chatroom"
-    
-    
+
     
--- PageObjectModel/Pages/AISearch.py
+++ PageObjectModel/Pages/AISearch.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 from PageObjectModel.Locators.PageLocators import *
 from constant import *
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -10,8 +11,15 @@ class AISearch():
 
     def __init__ (self , driver):
         self.driver = driver
-        self.test_execute()
-        print("COMPLETED")
+        self.run_test()
+
+    def run_test(self):
+        try:
+            self.test_execute()
+            print("Test is completed")
+        except Exception as e:
+            print(f"Test failed due to error: {e}")
+   
 
     #Normal Chat
     def homepage_send_text(self):
@@ -72,9 +80,9 @@ class AISearch():
         self.select_search_option("Youtube")
         self.chatroom_send_text("top 10 ai for 2024")
         self.chatroom_click_send_icon()
-        time.sleep(60)
+        time.sleep(70)
     
-    #GenerataeImage
+    #Generatae Image
     def chatroom_click_select_menu(self):
         self.driver.find_element(By.ID , Locators.chatroom_select_menu_icon).click()
     def chatroom_generate_image(self):
@@ -85,15 +93,50 @@ class AISearch():
     def test_generate_image(self):
         self.chatroom_click_select_menu()
         self.chatroom_generate_image()
-        time.sleep(2)
         self.chatroom_click_html()
-        time.sleep(2)
         self.chatroom_send_text("Best AI Logo 2024")
         self.chatroom_click_send_icon()
-        time.sleep(40)
+        time.sleep(40)    
+
+    #Suggested Replies (Disabled)
+    def chatroom_click_off_suggested_replies(self):
+        self.driver.find_element(By.ID , Locators.chatroom_suggested_replies).click()
+
+    def test_off_suggested_replies(self):
+        self.chatroom_click_select_menu()
+        self.chatroom_generate_image()
+        self.chatroom_click_off_suggested_replies()
+        self.chatroom_click_html()
+        self.chatroom_select_ai_search_button()
+        self.select_search_option("Salina Assistant")
+        self.chatroom_send_text("Can you please give me a top 5 ai tools? and what are the main features each of one.")
+        self.chatroom_click_send_icon()
+        time.sleep(60)
+
+    #File Document Upload
+    def chatroom_click_select_attachment(self):
+        attachment = self.driver.find_element(By.XPATH , "//button[@id='attachments-button-chatroom']")
+        self.driver.execute_script("arguments[0].focus()",attachment)
+        attachment.click()
+    def chatroom_click_file_button(self):
+        self.driver.find_element(By.ID , Locators.chatroom_select_file_document).click()
+    def upload_process(self):
+        fineinput = self.driver.find_element(By.NAME , Locators.chatroom_file_input)
+        self.file_path = AI_SEARCH_DOCUMENTS_ATTACHMENT
+        fineinput.send_keys(self.file_path)
+    def click_upload_button(self):
+        self.driver.find_element(By.ID , Locators.chatroom_upload_button).click()
+    
+    def test_file_upload(self):
+        self.chatroom_click_select_attachment()
+        self.chatroom_click_file_button()
+        self.upload_process()
+        self.click_upload_button()
+        time.sleep(80)
 
 
-    #Final Execution
+
+    #Test Execution
     def test_execute(self):
         self.test_normal_chat()
         self.test_web_search()
@@ -101,6 +144,8 @@ class AISearch():
         self.test_reddit_search()
         self.test_youtube_search()
         self.test_generate_image()
+        self.test_off_suggested_replies()
+        self.test_file_upload()
 
 
     
